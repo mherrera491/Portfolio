38241c00ed35ea943fe4cb77c352c4ffb7317097
mherrera491 <mattherrera1@gmail.com>
2024-02-13 23:34:09 -0800

    removed bootstrap

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap'
 
 export default function Footer(props) {
-    return (
-        <footer className="mt-5 p-3 bg-dark text-white">
-          <Container>
-            <Row>
-              <Col>
-                &copy; 2024 Matthew Herrera
-              </Col>
-            </Row>
-          </Container>
-        </footer>
-      );
+    return <h1>Footer</h1>
 }
